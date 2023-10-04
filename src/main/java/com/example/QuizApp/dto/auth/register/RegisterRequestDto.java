1aa7a7326459d46b6b403f93cd4a5286e975c061
MahmoudMatar <mahmoudmatar49@gmail.com>
2023-10-04 03:04:08 -0700

    خدي يا ام لسان

--- src/main/java/com/example/QuizApp/dto/auth/register/RegisterRequestDto.java
+++ src/main/java/com/example/QuizApp/dto/auth/register/RegisterRequestDto.java
@@ -9,5 +9,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequestDto {
     private String userNameOrEmail;
-    private String password;
+    private String password;//
 }
